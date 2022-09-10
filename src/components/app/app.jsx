37033b9bee4df7cab65401349647514bd7333104
Alexander Tsvetkov <hyehaeng@gmail.com>
2022-09-10 21:48:53 +0300

    refactoring usefetch hook

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,17 +5,17 @@ import BurgerConstructor from './../burger-constructor/burger-constructor';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 
 import { useFetch } from '../../hooks/use-fetch';
+import api from '../../utils/burger-api';
 
 import styles from './app.module.css';
 
 function App() {
-
 	const { 
 		data: ingredients, 
 		loading, 
 		error, 
 		execute: executeIngredients 
-	} = useFetch('ingredients');
+	} = useFetch(api.getIngredients);
 
 	useEffect(()=>{
 		executeIngredients();
--- src/hooks/use-fetch.js
+++ src/hooks/use-fetch.js
@@ -1,26 +1,30 @@
 import { useState } from 'react';
-import config from '../config/apiConfig';
 
-export function useFetch(requestMethod){
+import PropTypes from 'prop-types';
+
+function useFetch(request){
 	const [data, setData] = useState(void 0);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(false);
 
+	const checkReponse = (res) => {
+		return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
+	  };
+
 	function execute(){
 			setLoading(true);
 			setError(false);
-			fetch(config.url + requestMethod)
-			.then(res => res.json())
+			fetch(request.url, request.options)
+			.then(checkReponse)
 			.then(res => {
 				setData(res.data);
-				setLoading(false);
 				setError(false);
 			})
 			.catch(error => {
 				console.log(error);
-				setLoading(false);
 				setError(true);
-			});
+			})
+			.finally(() => setLoading(false));
 	};
 
 	return {
@@ -30,3 +34,18 @@ export function useFetch(requestMethod){
 		execute
 	}
 }
+
+useFetch.propTypes = {
+	request: PropTypes.shape({
+		url: PropTypes.string.isRequired,
+		options: PropTypes.shape({
+			method: PropTypes.oneOf(['GET', 'POST', 'PUT', 'DELETE']),
+			headers: PropTypes.oneOf([
+				"'Content-Type': 'application/json'", 
+				"'Content-Type': 'application/x-www-form-urlencoded'"]),
+			body: PropTypes.string
+		})
+	}).isRequired
+}
+
+export { useFetch } 
