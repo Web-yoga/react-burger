e608391b8d3df46eb0efd6b85684df3a8c77dbe4
Alexander Tsvetkov <hyehaeng@gmail.com>
2022-09-28 12:42:18 +0300

    ingredient menu scroll tracking

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import BurgerIngredientsSection from '../burger-ingredients-section/burger-ingredients-section';
 import { useSelector } from 'react-redux';
@@ -6,6 +6,11 @@ import { useSelector } from 'react-redux';
 import styles from './burger-ingredients.module.css';
 
 function BurgerIngredients() {
+
+	const ingredientMenuRef = useRef(null);
+	const bunTitleRef = useRef(null);
+	const sauceTitleRef = useRef(null);
+	const mainTitleRef = useRef(null);
 	
 	const INGREDIENT_TYPES = {
 		BUN: 'bun', 
@@ -17,10 +22,28 @@ function BurgerIngredients() {
 
 	const {ingredients, loading, error} = useSelector(state => state.ingredients);
 
+const handleScroll = event => {
+	const menuTop = ingredientMenuRef.current.getBoundingClientRect().top;
+	const bunTop = Math.abs(bunTitleRef.current.getBoundingClientRect().top - menuTop);
+	const sauceTop = Math.abs(sauceTitleRef.current.getBoundingClientRect().top - menuTop);
+	const mainTop = Math.abs(mainTitleRef.current.getBoundingClientRect().top - menuTop);
+
+	if(bunTop < sauceTop){
+		setCurrent(INGREDIENT_TYPES.BUN);
+	}else if(sauceTop < mainTop){
+		setCurrent(INGREDIENT_TYPES.SAUCE);
+	}else{
+		setCurrent(INGREDIENT_TYPES.MAIN);
+	}
+}
+
 	return (
 		<div className={ `${styles.container} mr-10` }>
 			<h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
-			<section className={styles.tabs}>
+			<section 
+				ref={ingredientMenuRef}
+				className={styles.tabs}
+			>
     		  <Tab 
 			  value={INGREDIENT_TYPES.BUN} 
 			  active={current === INGREDIENT_TYPES.BUN} 
@@ -40,7 +63,10 @@ function BurgerIngredients() {
     		    Начинки
     		  </Tab>
     		</section>
-			<section className={styles.ingredientsList}>
+			<section 
+				className={styles.ingredientsList}
+				onScroll={handleScroll}
+			>
 				{ loading && <p>Loading!</p> }
 				{ error && <p>Error!</p> }
 				{ 
@@ -48,14 +74,17 @@ function BurgerIngredients() {
 				&& 
 				<>
 				<BurgerIngredientsSection
+					ref={bunTitleRef}
 					ingredients = {ingredients}
 					filter={INGREDIENT_TYPES.BUN}
 					title='Булки' />
 				<BurgerIngredientsSection
+					ref={sauceTitleRef}
 					ingredients = {ingredients}
 					filter={INGREDIENT_TYPES.SAUCE}
 					title='Соусы' />
 				<BurgerIngredientsSection
+					ref={mainTitleRef}
 					ingredients = {ingredients}
 					filter={INGREDIENT_TYPES.MAIN}
 					title='Начинки' />	
