2d51c3ac3b78ee79d24db39b7a62beb293be9365
Alexander Tsvetkov <hyehaeng@gmail.com>
2022-10-15 15:13:48 +0300

    getIngredients on detail page and remove curentIngredient state

--- src/pages/ingredients.jsx
+++ src/pages/ingredients.jsx
@@ -1,34 +1,12 @@
 import { useEffect } from 'react';
-import { useParams } from 'react-router';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import AppHeader from './../components/app-header/app-header';
 import IngredientDetails from './../components/ingredient-details/ingredient-details';
-import { SET_CURRENT_INGREDIENT } from './../services/actions/current-ingredient';
 import { getIngredients } from '../services/actions/get-ingredients';
 
 export function IngredientsPage() {
-	const { id } = useParams();
-	const { loading, error, ingredients } = useSelector(state => state.ingredients);
-
 	const dispatch = useDispatch();
-
-	useEffect(() => {
-		if(ingredients.length > 0){
-			const ingredient = ingredients.find(item => item._id === id);
-			if(ingredient){
-				dispatch({
-					type: SET_CURRENT_INGREDIENT,
-					payload: ingredient
-				});
-			} else {
-				dispatch({
-					type: SET_CURRENT_INGREDIENT,
-					payload: 'not found'
-				});
-			}
-		}
-	}, [id, ingredients, dispatch]);
 	
 	useEffect( () => {
 		dispatch(getIngredients());
@@ -38,8 +16,6 @@ export function IngredientsPage() {
 		<div className='app'>
 			<AppHeader/>
 			<main className='container--center'>
-				{ loading && <p>Загрузка...</p> }
-				{ error && <p>Произошла ошибка!</p> }
 				<IngredientDetails />
 			</main>
 		</div>
--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from "redux";
 import { authReducer } from "./auth"; 
 import { getIngredientsReducer } from "./get-ingredients"; 
 import { constructorIngredientsReducer } from "./constructor-ingredients"; 
-import { currentIngredientReducer } from "./current-ingredient"; 
 import { orderReducer } from "./order"; 
 
 export const rootReducer = combineReducers({
 	auth: authReducer,
 	ingredients: getIngredientsReducer,
 	constructorIngredients: constructorIngredientsReducer,
-	currentIngredient: currentIngredientReducer,
 	order: orderReducer
 });
